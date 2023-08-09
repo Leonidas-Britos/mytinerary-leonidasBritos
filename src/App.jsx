000d5c11f8689ab6dd5b23996539f1431fa7eefd
Leonidas <britos659@gmail.com>
2023-08-09 05:33:26 -0300

    Cambios Estéticos

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,6 @@
 import "./css/App.css";
-<<<<<<< HEAD
 import Footer from "./layouts/Footer";
 import Header from "./layouts/Header";
-=======
-import MainLayoutFooter from "./layouts/footer/MainLayoutFooter";
-import MainLayout from "./layouts/header/MainLayoutHeader";
->>>>>>> 693ddd4eda016b164111cb3a34cfb25074f4a4eb
 import ContentHome from "./pages/ContentHome";
 
 function App() {
@@ -19,11 +14,7 @@ function App() {
       </main>
       {/*Footer*/}
       <footer>
-<<<<<<< HEAD
       <Footer></Footer>
-=======
-      <MainLayoutFooter></MainLayoutFooter>
->>>>>>> 693ddd4eda016b164111cb3a34cfb25074f4a4eb
       </footer>
       
       
--- src/components/header/Navbar.jsx
+++ src/components/header/Navbar.jsx
@@ -1,14 +1,20 @@
 export default function Navbar() {
   return (
     <nav className="navPage">
-          <a className="anchorNav" href="/#/">
-            Home
-          </a>
-          <a className="anchorNav" href="/#/">
-            Cities
-          </a>
+          <div className="buttonHomeNav">
+            <img className="iconHomeNav"/>
+            <a className="textButtonHome" href="/#/">
+              Home
+            </a>
+          </div>
+          <div className="buttonCities">
+            <img className="iconCitiesnNav" src="./public/img/city.png" alt="" />
+            <a className="textButtonCities" href="/#/">
+              Cities
+            </a>
+          </div>
           <div className="buttonLogin">
-            <img className="iconLoginNav" src="./public/img/user.png" alt="" />
+            <img className="iconLoginNav" src="./public/img/user2.png" alt="" />
             <a className="textButtonLogin" href="/#/">
               Login
             </a>
