c19676cebccaaebda5446a04ecb6c2888e33fb4e
Leonidas <britos659@gmail.com>
2023-08-22 20:31:08 -0300

    avance de carousel funcionando dinamicamente con backend

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,23 +1,9 @@
-import { useState, useEffect } from "react";
 import CarouselCard from "./CarouselCard";
-import axios from "axios";
+import { useState } from "react";
 
-export default function Carousel() {
-  useEffect(
-    () => {
-      axios("/data.json")
-        .then((res) => setData(res.data))
-        .catch((err) => console.log(err));
-    }, //callback que NO debe retornar nada y NO puede ser asincrona
-    [] //array de dependencias
-    //cuando está vacío EL EFECTO se ejecuta UNA UNICA VEZ cuando se monta el componente
-    //cuando tiene variables de dependencias EL EFECTO se ejecuta (cuando se monta y) CADA VEZ que varía/cambia alguna de esas variables
-    //[show]  //en este ejemplo CADA VEZ que el booleano CAMBIE, se ejecuta la caalback de la L28
-  );
-
+export default function Carousel({ data }) {
   let [counter, setCounter] = useState(0);
   let [counterTo, setCounterTo] = useState(4);
-  const [data, setData] = useState([]);
   function next_slide() {
     if (data.length <= counterTo) {
       setCounter(0);
