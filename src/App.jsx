3cd61ba42f211ed1e8281224050c75d7d7bc1ccb
Leonidas <britos659@gmail.com>
2023-08-25 03:23:19 -0300

    redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import "./css/App.css";
 import { RouterProvider } from 'react-router-dom';
 import router from './router';
+import { Provider } from "react-redux";
+import store from "./store/store";
 
 
 function App() {
   return (
-    <RouterProvider router={router} />
-  );
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  )
 }
 
 export default App;
