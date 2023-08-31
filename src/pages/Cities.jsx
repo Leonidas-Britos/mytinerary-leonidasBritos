ee815f16a0a6bf60ccd3baa807ce05415edac0c0
Leonidas <britos659@gmail.com>
2023-08-31 03:06:54 -0300

    Mostrando titulos de itineraries por cities

--- src/pages/Cities.jsx
+++ src/pages/Cities.jsx
@@ -20,9 +20,14 @@ export default function Cities() {
     setReEffect(!reEffect)
   }
   return (
+    <div className='w-[100%] flex flex-wrap justify-between	py-[7%] border-solid border-black border-2 gap-[3rem]'>
     <>
-    <input ref={text} type="text" name="text" id="text" onKeyUp={handleFilter} />
+    <input className='border-[0.125rem] border-black border-2 rounded-[0.3rem] w-[30%] h-[2.5rem]' ref={text} type="text" name="text" id="text" onKeyUp={handleFilter} />
+    <div className='flex flex-wrap justify-between gap-[3rem] px-[5%]'>
     {cities.map(each=><CardCity key={each._id} src={each.photo} alt={each._id} text={each.city} id={each._id} />)}
+    </div>
+    
     </>
+    </div>
   )
 }
