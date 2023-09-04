6587bb42a90a0d8aa6424601ec280777230b0b5b
Leonidas <britos659@gmail.com>
2023-09-04 07:31:14 -0300

    arreglos en responsive

--- src/components/DetailCity.jsx
+++ src/components/DetailCity.jsx
@@ -19,32 +19,53 @@ export default function DetailCity({ src, alt, tittle, id, description }) {
   return (
     <Anchor to={"/city/" + id}>
       <div id="detailCityContent" className="w-[auto] h-[auto] mx-auto flex flex-col justify-space-between bg-[#EBEBEB] 
-        sm: w-[42rem] h-[28rem]
-        md: w-[46rem] h-[30rem]
-        lg:
-        xl:w-[98vw] h-[50rem]">
+        sm:w-[42rem] sm:h-[28rem]
+        lg:w-[auto] xl:h-[auto] mx-auto flex flex-col justify-space-between bg-[#EBEBEB]
+        xl:w-[auto] xl:h-[auto] mx-auto flex flex-col justify-space-between bg-[#EBEBEB] ">
+
         <img id="detailCityPhoto" src={src} alt={alt} className="w-[100%] h-[10rem] border-[#94A3B8] border-solid border-[0.4rem]
-          sm:w-[100%] h-[35rem]
-          xl:w-[100%] h-[10rem] border-[#94A3B8] border-solid border-[0.4rem]"/>
+          sm:w-[100%] sm:h-[35rem]
+          lg:w-[100%] lg:h-[auto] 
+          xl:w-[100%] xl:h-[auto] border-[#94A3B8] border-solid border-[0.4rem] "/>
+
         <div id="boxDetailCityTittleAndDescription" className="w-auto h-[auto] px-[15%] gap-[1rem] flex flex-col 
-          xl:w-auto h-[auto] px-[15%] gap-[1rem] flex flex-col">
+          lg:w-auto lg:h-[40rem] px-[15%] gap-[1rem] flex flex-col
+          xl:w-auto xl:h-[auto] px-[15%] gap-[1rem] flex flex-col">
+
           <p id="detailCityText" className="text-center text-2xl font-semibold 
-            md:text-4xl">
+            md:text-4xl
+            xl:text-center text-2xl font-semibold ">
             {tittle}
           </p>
           <p id="detailCityDescription" className="">
             {description}
           </p>
-          <button id="buttonViewHideItineraries" onClick={() => setShow(!show)} className="w-[auto] h-[auto] p-[1rem] mx-auto my-[2.2rem] text-[1.3rem] border-2 border-solid border-black rounded-lg font-semibold text-[1.4rem] bg-[#4f46e5] cursor-pointer text-white 
-          md: w-[10rem] h-[5.5rem] text-[1.6rem] hover:bg-[#a97aff] hover:text-[black]">
+
+          <button id="buttonViewHideItineraries" onClick={() => setShow(!show)} className="w-[auto] h-[auto] p-[1rem] mx-auto my-[2.2rem] text-[1.3rem] border-2 border-solid border-black rounded-lg font-semibold text-[1.4rem] bg-[#4f46e5] cursor-pointer text-white hover:bg-[#a97aff] hover:text-[black]
+          md:w-[10rem] md:h-[5.5rem] text-[1.6rem] 
+          lg:w-[auto] lg:h-[auto] p-[1rem] mx-auto my-[2.2rem] text-[1.3rem] border-2 border-solid border-black rounded-lg font-semibold text-[1.4rem] bg-[#4f46e5] cursor-pointer text-white
+          xl:w-[auto] xl:h-[auto] p-[1rem] mx-auto my-[2.2rem] text-[1.3rem] border-2 border-solid border-black rounded-lg font-semibold text-[1.4rem] bg-[#4f46e5] cursor-pointer text-white">
             {show ? 'Hide Itineraries ↑' : 'View Itineraries ↓'}</button>
         </div>
-        <div id="boxViewItineraries" className="w-[auto] h-[auto] gap-[4rem] mb-[2rem] flex flex-col items-center bg-[#EBEBEB]">
+        
+        <div id="boxViewItineraries" className="w-[auto] h-[auto] gap-[4rem] mb-[2rem] flex flex-col items-center bg-[#EBEBEB]
+        lg:lg:w-[auto] lg:h-[auto] gap-[4rem] mb-[2rem] flex flex-col items-center bg-[#EBEBEB]
+        xl:w-[auto] xl:h-[auto] gap-[4rem] mb-[2rem] flex flex-col items-center bg-[#EBEBEB]">
           {show && itineraries.map(itinerary =>
-            <div id="ViewitineraryCard" key={itinerary._id} className="w-[50rem] h-[auto] pb-[1rem] flex flex-col items-center justify-around border-solid border-[#94A3B8] border-[0.2rem] rounded-[1rem] bg-[#fdfdfd]">
+          
+            <div id="ViewitineraryCard" key={itinerary._id} className="w-[50rem] h-[auto] pb-[1rem] flex flex-col items-center justify-around border-solid border-[#94A3B8] border-[0.2rem] rounded-[1rem] bg-[#fdfdfd]
+            lg:w-[50rem] lg:h-[auto] pb-[1rem] flex flex-col items-center justify-around border-solid border-[#94A3B8] border-[0.2rem] rounded-[1rem] bg-[#fdfdfd]
+            xl:w-[50rem] xl:h-[auto] pb-[1rem] flex flex-col items-center justify-around border-solid border-[#94A3B8] border-[0.2rem] rounded-[1rem] bg-[#fdfdfd]">
+              
               <p key={itinerary._id} className="text-[2rem]">{itinerary.name}</p>
-              <img id="viewItineraryPhoto" src={itinerary.photo} alt={itinerary.name} className="w-[auto] h-[auto] px-[3rem] rounded-[1rem]" />
-              <div id="viewItineraryDetails" className="w-[100%] px-[3rem] py-[0.8rem] flex flex-row justify-between items-center rounded-[1rem]">
+              <img id="viewItineraryPhoto" src={itinerary.photo} alt={itinerary.name} className="w-[auto] h-[auto] px-[3rem] rounded-[1rem]
+             lg:w-[auto] lg:h-[auto] px-[3rem] rounded-[1rem]
+             xl:w-[auto] xl:h-[auto] px-[3rem] rounded-[1rem]" />
+              
+              <div id="viewItineraryDetails" className="w-[100%] px-[3rem] py-[0.8rem] flex flex-row justify-between items-center rounded-[1rem]
+              lg:w-[100%] px-[3rem] py-[0.8rem] flex flex-row justify-between items-center rounded-[1rem]
+              xl:w-[100%] px-[3rem] py-[0.8rem] flex flex-row justify-between items-center rounded-[1rem]">
+
                 <p id="iconHeart" className="gap-[0.5rem] flex text-[2rem] items-center"><svg xmlns="http://www.w3.org/2000/svg" fill="pink" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-[2.5rem]">
                   <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
                 </svg>
@@ -62,9 +83,13 @@ export default function DetailCity({ src, alt, tittle, id, description }) {
                 </button>
               </div>
               {showDetails ?
-                <div id="usersHashDurPrice" className="h-[3rem] w-[100%] mb-[2.5rem] flex flex-row justify-around content-between bg-[#fdfdfd]">
+                <div id="usersHashDurPrice" className="h-[3rem] w-[100%] mb-[2.5rem] flex flex-row justify-around content-between bg-[#fdfdfd]
+                lg:h-[3rem] lg:w-[100%] mb-[2.5rem] flex flex-row justify-around content-between bg-[#fdfdfd]
+                xl:h-[3rem] xl:w-[100%] mb-[2.5rem] flex flex-row justify-around content-between bg-[#fdfdfd] ">
                   <div id="userProfile">
-                    <p id="usertext" className="flex flex-col items-center text-[1.4rem] "><b>User:</b></p>
+                    <p id="usertext" className="flex flex-col items-center text-[1.4rem] 
+                    lg:flex flex-col items-center text-[1.4rem]
+                    xl:flex flex-col items-center text-[1.4rem] "><b>User:</b></p>
                     <img id="userPhoto" src="/public/img/user_example.png" alt="" />
                   </div>
                   <p className="flex flex-col items-center text-[1.4rem] "><b>Hashtags:</b>{itinerary.tags}</p>
@@ -72,9 +97,11 @@ export default function DetailCity({ src, alt, tittle, id, description }) {
                   <p className="flex flex-col items-center text-[1.4rem] "><b>Price: </b>${"💵".repeat(itinerary.price)}</p>
 
                 </div> : null}
-              <button id="buttonShowActivities" onClick={() => setShowActivities(!showActivities)} className="w-[auto] h-[auto]  mx-auto  p-[1rem] border-2 text-[1rem] border-solid border-black rounded-lg font-semibold text-[1.4rem] text-white  bg-[#4f46e5] cursor-pointerhover:bg-[#a97aff] hover:text-[black]">{showActivities ? 'Hide Activities ↑' : 'View Activities ↓'}</button>
+              <button id="buttonShowActivities" onClick={() => setShowActivities(!showActivities)} className="w-[auto] h-[auto]  mx-auto  p-[1rem] border-2 text-[1rem] border-solid border-black rounded-lg font-semibold text-[1.4rem] text-white  bg-[#4f46e5] cursor-pointer hover:bg-[#a97aff] hover:text-[black]">{showActivities ? 'Hide Activities ↑' : 'View Activities ↓'}</button>
               {showActivities ?
-                <p id="pUnderConstruction" className="p-[1rem] mt-[1rem] text-[1.4rem] text-[green] font-semibold border-double border-[0.5rem] border-[green]">Sorry! This element is under Construction :)</p>
+                <p id="pUnderConstruction" className="p-[1rem] mt-[1rem] text-[1.4rem] text-[green] font-semibold border-double border-[0.5rem] border-[green]
+                lg:p-[1rem] mt-[1rem] text-[1.4rem] text-[green] font-semibold border-double border-[0.5rem] border-[green]
+                xl:p-[1rem] mt-[1rem] text-[1.4rem] text-[green] font-semibold border-double border-[0.5rem] border-[green]">Sorry! This element is under Construction :)</p>
                 : null}
             </div>
           )}
